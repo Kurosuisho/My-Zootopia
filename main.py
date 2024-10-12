c146b78fce7e54df9372a0228ad472afb62b855f
Kurosuisho <kurosuisho@proton.me>
2024-10-12 15:51:43 +0200

    removing placeholder, replacing with actual data

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ def load_data(file_path):
 
 
 def main():
-    animals_data = load_data('animals_data.json')
+    animals_data = load_data("animals_data.json")
+    output = ""
     for animal in animals_data:
         if "type" in animal["characteristics"]:
             name = animal["name"]
             diet = animal["characteristics"]["diet"]
             animal_type = animal["characteristics"]["type"]
             locations = animal["locations"][0]
-            print(f"Name: {name}\nDiet: {diet}\nLocation: {locations}\nType: {animal_type}\n")
+            output += f"Name: {name}\n"
+            output += f"Diet: {diet}\n"
+            output += f"Location: {locations}\n"
+            output += f"Type: {animal_type}\n"
+
+    with open("animals_template.html", "r") as html_file:
+        html_content = html_file.read()
+
+    modified_html = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w") as modified_file:
+        modified_file.write(modified_html)
 
 
 if __name__ == "__main__":
